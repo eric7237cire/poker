62484c467f8f5a8cdf02c93e6721d9ed47293e3f
eg <eric7237cire@gmail.com>
2017-12-18 21:53:30 +0100

    wip

--- get_screenshot.py
+++ get_screenshot.py
@@ -1,16 +1,21 @@
 import os
-from PIL import ImageGrab
+
+if os.name == 'nt':
+    from PIL import ImageGrab
 import win32gui
 from PIL import Image, ImageDraw
 
 toplist, winlist = [], []
+
+
 def enum_cb(hwnd, results):
     winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
+
 
 def capture_screenshot(windows_title, output_file_path):
     win32gui.EnumWindows(enum_cb, toplist)
 
-    #print(winlist)
+    # print(winlist)
 
     matching_hwnd_list = [(hwnd, title) for hwnd, title in winlist if windows_title.lower() in title.lower()]
     # just grab the hwnd for first window matching matching_hwnd_list
@@ -20,9 +25,8 @@ def capture_screenshot(windows_title, output_file_path):
     win32gui.SetForegroundWindow(hwnd)
     bbox = win32gui.GetWindowRect(hwnd)
 
-    #print(bbox)
+    # print(bbox)
     img = ImageGrab.grab(bbox)
     # img.show()
-
 
     img.save(output_file_path)

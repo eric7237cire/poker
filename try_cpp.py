bd5b1252fd90ff17e8b3903bcf4b327201646f4a
eg <eric7237cire@gmail.com>
2017-12-24 11:22:14 +0100

    closer, return a numpy array

--- try_cpp.py
+++ try_cpp.py
@@ -2,6 +2,17 @@ import poker
 
 #poker.run_simulation(4, "AdKd", "QdJdTd", 1)
 
-equity = poker.run_simulation(4, "6d3s", "", 1000000)
+#equity = poker.run_simulation(4, "6d3s", "", 1000000)
 
-print(f"My equity is {equity}")
+#print(f"My equity is {equity}")
+
+test_array = poker.take_screenshot()
+
+print(test_array.shape)
+print(test_array.dtype)
+print(test_array)
+
+print(test_array[49][19])
+print(test_array[48][18])
+
+
